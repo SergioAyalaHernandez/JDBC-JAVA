7a06b0242d72838a7a89ffa2290a750d05bd00bc
Sergio <sergio.ayala9208@gmail.com>
2024-01-21 16:54:08 -0500

    se agregan cambios y una interfaz grafica, para visualizar los metodos de manera interactiva, se usa un pool, para el uso de las conexiones de bases de datos

--- src/main/java/org/example/main/Main.java
+++ src/main/java/org/example/main/Main.java
@@ -1,22 +1,56 @@
 package org.example.main;
 
+import org.example.model.Employee;
+import org.example.repository.EmployeeRepository;
+import org.example.repository.Repository;
 import org.example.util.DatabaseConnection;
+import org.example.view.SwingApp;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 
 public class Main {
     public static void main(String[] args) throws SQLException {
 
-        try (Connection myCon = DatabaseConnection.getInstance();
-             Statement myStamt = myCon.createStatement();
-             ResultSet myRes = myStamt.executeQuery("select * from Employee");
-        ) {
-            while (myRes.next()) {
-                System.out.println(myRes.getString("firstname"));
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-            System.out.println("algo mal");
-        }
+       /* try (Connection myCon = DatabaseConnection.getInstance()) {
+            Repository<Employee> repository = new EmployeeRepository();
+
+
+            System.out.println("<-------------Probando el segundo metodo, getById--------->");
+            System.out.println(repository.getById(1));
+
+            System.out.println("<------------Probando el metodo de inserción---------->");
+            Employee employee = new Employee();
+            employee.setId(11);
+            employee.setFirstName("Sergio Ismael");
+            employee.setFirstSurname("Ayala");
+            employee.setSecondSurname("Hernandez");
+            employee.setEmail("SergioAyala9208@gmail.com");
+            employee.setSalary(6000000.0F);
+            repository.save(employee);
+
+            System.out.println("<------------Probando el metodo de inserción---------->");
+            Employee employee2 = new Employee();
+            employee2.setFirstName("Linda");
+            employee2.setFirstSurname("Sarmiento");
+            employee2.setSecondSurname("Camacho");
+            employee2.setEmail("Linda@gmail.com");
+            employee2.setSalary(6000000.0F);
+            repository.save(employee2);
+
+            System.out.println("<----------------Listado todo--------------->");
+            repository.findAll().forEach(System.out::println);
+
+            repository.delete(12);
+
+            System.out.println("<----------------Listado todo--------------->");
+            repository.findAll().forEach(System.out::println);
+
+        }*/
+
+        SwingApp app = new SwingApp();
+
+        app.setVisible(true);
     }
 }
--- src/main/java/org/example/repository/EmployeeRepository.java
+++ src/main/java/org/example/repository/EmployeeRepository.java
@@ -1,4 +1,87 @@
 package org.example.repository;
 
-public class EmployeeRepository {
+import org.example.model.Employee;
+import org.example.util.DatabaseConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeRepository implements Repository<Employee> {
+
+
+    private Connection getConnection() throws SQLException {
+        return DatabaseConnection.getConnection();
+    }
+
+    @Override
+    public List<Employee> findAll() throws SQLException {
+        List<Employee> employees = new ArrayList<>();
+        try (Connection myCoon = getConnection(); Statement myStamt = myCoon.createStatement(); ResultSet myRes = myStamt.executeQuery("SELECT * FROM employee")) {
+            while (myRes.next()) {
+                Employee employee = createEmployees(myRes);
+                employees.add(employee);
+            }
+        }
+        return employees;
+    }
+
+
+    @Override
+    public Employee getById(Integer id) throws SQLException {
+        Employee employee = null;
+        try (Connection myCoon = getConnection(); PreparedStatement myStamt = getConnection().prepareStatement("SELECT * FROM employee WHERE id = ?")) {
+            myStamt.setInt(1, id);
+            try (ResultSet myRes = myStamt.executeQuery()) {
+                if (myRes.next()) {
+                    employee = createEmployees(myRes);
+                }
+            }
+        }
+        return employee;
+    }
+
+    @Override
+    public void save(Employee employee) throws SQLException {
+        String sql;
+        if (employee.getId() != null && employee.getId() > 0) {
+            sql = "UPDATE employee SET firstName = ?, firstSurname = ?, SecondSurname = ?, email = ?, salary = ? WHERE id = ?";
+        } else {
+            sql = "INSERT INTO employee (firstName,firstSurname,secondSurname,email,salary) VALUES(?,?,?,?,?)";
+        }
+
+
+        try (Connection myCoon = getConnection(); PreparedStatement myStamt = getConnection().prepareStatement(sql)) {
+            myStamt.setString(1, employee.getFirstName());
+            myStamt.setString(2, employee.getFirstSurname());
+            myStamt.setString(3, employee.getSecondSurname());
+            myStamt.setString(4, employee.getEmail());
+            myStamt.setFloat(5, employee.getSalary());
+            if (employee.getId() != null && employee.getId() > 0) {
+                myStamt.setInt(6, employee.getId());
+            }
+            myStamt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(Integer id) throws SQLException {
+        try (Connection myCoon = getConnection(); PreparedStatement myStamt = getConnection().prepareStatement("DELETE FROM employee WHERE id = ?")) {
+            myStamt.setInt(1, id);
+            myStamt.executeUpdate();
+        }
+    }
+
+    private Employee createEmployees(ResultSet myRes) throws SQLException {
+        Employee e = new Employee();
+        e.setId(myRes.getInt("id"));
+        e.setFirstName(myRes.getString("firstName"));
+        e.setFirstSurname(myRes.getString("firstSurname"));
+        e.setSecondSurname(myRes.getString("secondSurname"));
+        e.setEmail(myRes.getString("email"));
+        e.setSalary(myRes.getFloat("salary"));
+        return e;
+    }
 }
